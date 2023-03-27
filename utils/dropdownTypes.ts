26eaab1e0fa17a8c9bc2c06a982312d3a861a1d4
zweetson <zweetson@gmail.com>
2023-03-27 04:30:43 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ utils/dropdownTypes.ts
@@ -0,0 +1,38 @@
+export type themeType =
+  | "Modern"
+  | "Vintage"
+  | "Minimalist"
+  | "Professional"
+  | "Tropical"
+  | "Industrial"
+  | "Neoclassic";
+
+export type roomType =
+  | "Living Room"
+  | "Dining Room"
+  | "Bedroom"
+  | "Bathroom"
+  | "Office"
+  | "Kitchen"
+  | "Basement"
+  | "Gaming Room";
+
+export const themes: themeType[] = [
+  "Modern",
+  "Minimalist",
+  "Professional",
+  "Tropical",
+  "Vintage",
+  "Industrial",
+  "Neoclassic",
+];
+export const rooms: roomType[] = [
+  "Living Room",
+  "Dining Room",
+  "Office",
+  "Bedroom",
+  "Bathroom",
+  "Basement",
+  "Kitchen",
+  "Gaming Room",
+];
